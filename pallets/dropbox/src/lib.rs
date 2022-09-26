baa593e5e019664df3477ed21584825f142f48a5
kaioh33 <gupajibril@gmail.com>
2022-09-26 20:37:02 +0100

    add dispatchables

--- pallets/dropbox/src/lib.rs
+++ pallets/dropbox/src/lib.rs
@@ -16,7 +16,7 @@ pub mod pallet {
 		traits::{Currency, ExistenceRequirement, Randomness},
 		transactional,
 	};
-	use frame_system::pallet_prelude::*;
+	use frame_system::pallet_prelude::{*, OriginFor};
 
 	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 	#[scale_info(skip_type_params(T))]
@@ -96,41 +96,19 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
+		#[pallet::weight(100)]
+		pub fn upload_file(origin: OriginFor<T>, ) -> DispatchResult {
 
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		#[pallet::weight(100)]
+		pub fn download_file(origin: OriginFor<T>) -> DispatchResult {
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
+		}
+
+		#[pallet::weight(100)]
+		pub fn transfer_file(origin: OriginFor<T>) -> DispatchResult {
+			
 		}
 	}
 }
